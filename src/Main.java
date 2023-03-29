296d447bf15d45239723a4efac6430ed81893255
Marcel Rohrbach <108031921+Mysthic1975@users.noreply.github.com>
2023-03-29 11:30:44 +0200

    neue Version

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,39 @@
-import java.util.Scanner;
-
-public class Main {
+public class Main
+{
     public static void main(String[] args)
     {
-        Scanner scanner = new Scanner(System.in);
+        int A, B;                                                           // Hilfsvariablen zum Rechnen
+        float C, D;                                                         // Komma
+        double E, F;                                                        // Komma
+        int Summe;                                                          // Addition
+        int intDifference;                                                  // Subtraktion
+        float floatDifference;                                              // Subtraktion mit Komma
+        int Product;                                                        // Multiplikation
+        int Qoutient;                                                       // Division
 
-        System.out.println("Einfacher Taschenrechner");
-        System.out.print("Erste Zahl eingeben: ");
-        int ersteZahl = scanner.nextInt();
+        A = 10;
+        B = 5;
+        C = 4.4f;
+        D = 8.8f;
+        E = 10.5;
+        F = 5.5;
 
-        System.out.print("Zweite Zahl eingeben: ");
-        int zweiteZahl = scanner.nextInt();
+        Summe = A + B;
+        intDifference = A - B;
+        floatDifference = D - C;
+        double Ergebnis = E - F;
+        Product = A * B;
+        Qoutient = A / B;
 
-        int ergebnis = ersteZahl + zweiteZahl;
-        System.out.println("Ergebnis: " + ergebnis);
+        System.out.println ("A :" + A);                                     // Kommentar
+        System.out.println ("A + A: " + A + A);
+        System.out.println ("Addition: " + Summe);
+        System.out.println ("Subtraktion: " + intDifference);
+        System.out.println ("Float Subtraktion: " + floatDifference );
+        System.out.println ("Double Subtraktion: " + Ergebnis);
+        System.out.println ("Multiplikation: " + Product);
+        System.out.println ("Division: " + Qoutient);
+        System.out.println ("Hello world!");
+
     }
 }
