a38463dc885c1f614331776b49f0028aec76b281
Marcel Rohrbach <108031921+Mysthic1975@users.noreply.github.com>
2023-03-31 11:45:09 +0200

    Update Main.java
    Array

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner eingabe = new Scanner(System.in);
         int a, b;
-        int ErgebnisGanzzahl;
-        char Operator;
-        double ErgebnisKommazahl;
-        double c, d;
+        double ergebnis;
+        char operator;
         String fortsetzen;
+        double[] ergebnisHistorie = new double[10];
+        int historieIndex = 0;
 
         do {
             System.out.println("Taschenrechner");
@@ -24,40 +24,55 @@ public class Main {
                 eingabe.next();
             }
             b = eingabe.nextInt();
+
             System.out.print("Rechenoperation auswählen (+, -, *, /): ");
-            Operator = eingabe.next().charAt(0);
+            operator = eingabe.next().charAt(0);
 
-            if (Operator == '+') {
-                ErgebnisGanzzahl = a + b;
-                System.out.println("Das Ergebnis von " + a + " + " + b + " ist: " + ErgebnisGanzzahl);
-            } else if (Operator == '-') {
-                ErgebnisGanzzahl = a - b;
-                System.out.println("Das Ergebnis von " + a + " - " + b + " ist: " + ErgebnisGanzzahl);
-            } else if (Operator == '*') {
-                ErgebnisGanzzahl = a * b;
-                System.out.println("Das Ergebnis von " + a + " * " + b + " ist: " + ErgebnisGanzzahl);
-            } else if (Operator == '/' && b != 0) {
-                c = Double.valueOf(a);
-                d = Double.valueOf(b);
-
-                ErgebnisKommazahl = c / d;
-                System.out.println("Das Ergebnis von " + a + " / " + b + " ist: " + ErgebnisKommazahl);
-            } else if (Operator == '/' && b == 0) {
-                System.out.println("Division durch Null nicht möglich!");
-            } else {
-                System.out.println("Ungültige Eingabe! Bitte wählen Sie eine gültige Rechenoperation (+, -, *, /).");
-            }
-
-            while (true) {
-                System.out.print("Möchtest du eine weitere Berechnung durchführen? (j/E): ");
-                fortsetzen = eingabe.next().toUpperCase();
-                if (fortsetzen.equals("J") || fortsetzen.equals("E")) {
-                    break;
+            char[] operationen = {'+', '-', '*', '/'};
+            for (char op : operationen) {
+                if (op == operator) {
+                    if (op == '+') {
+                        ergebnis = a + b;
+                        System.out.println("Das Ergebnis von " + a + " + " + b + " ist: " + ergebnis);
+                        ergebnisHistorie[historieIndex] = ergebnis;
+                        historieIndex++;
+                        break;
+                    } else if (op == '-') {
+                        ergebnis = a - b;
+                        System.out.println("Das Ergebnis von " + a + " - " + b + " ist: " + ergebnis);
+                        ergebnisHistorie[historieIndex] = ergebnis;
+                        historieIndex++;
+                        break;
+                    } else if (op == '*') {
+                        ergebnis = a * b;
+                        System.out.println("Das Ergebnis von " + a + " * " + b + " ist: " + ergebnis);
+                        ergebnisHistorie[historieIndex] = ergebnis;
+                        historieIndex++;
+                        break;
+                    } else if (b != 0) {
+                        ergebnis = (double) a / (double) b;
+                        System.out.println("Das Ergebnis von " + a + " / " + b + " ist: " + ergebnis);
+                        ergebnisHistorie[historieIndex] = ergebnis;
+                        historieIndex++;
+                        break;
+                    } else {
+                        System.out.println("Division durch Null nicht möglich!");
+                        break;
+                    }
                 }
             }
+
+            do {
+                System.out.print("Möchtest du eine weitere Berechnung durchführen? (j/E): ");
+                fortsetzen = eingabe.next().toUpperCase();
+            } while (!fortsetzen.equals("J") && !fortsetzen.equals("E"));
         } while (!fortsetzen.equals("E"));
 
-        // Nachricht am Ende des Programms ausgeben
+        System.out.println("Die letzten Ergebnisse waren:");
+        for (int i = 0; i < historieIndex; i++) {
+            System.out.println(ergebnisHistorie[i]);
+        }
+
         System.out.println("Danke für die Benutzung des Taschenrechners!");
     }
 }
